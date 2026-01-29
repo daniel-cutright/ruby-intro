4e9ee7fa61fd15e383c5c59f6ccee8bea8614224
daniel-cutright <daniel.cutright@kellogg.northwestern.edu>
2026-01-29 01:42:09 +0000

    Completed first 60 minutes of class

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -5,15 +5,42 @@
 # In Ruby, there are different *types* of data:
 
 # Numbers
+puts 3
+puts 2
+puts 2 + 3
+puts 2 - 3
+puts 2 * 3
+puts 2 / 3
+puts 2 % 3
+puts 2.0 / 3
 
 # Non-numbers
+puts "Go Pats!"
+puts "Riley is the best dog!"
 
 # True, False
+# puts true
+# puts false
 
 # Nothing
 
 # Variables
+x = 24
+y = 7
+z = 3
+puts x
+puts y
+puts z
+puts x / z
+
+food = "tacos"
+puts food
 
 # Combine strings and variables
+puts food + " are delish!"
+puts "#{food} are delish!"
+puts food * 3
 
 # String manipulation
+puts food.upcase
+puts food.reverse
--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -3,13 +3,51 @@
 # ruby 2-conditionals.rb
 
 # Booleans
+true
+false
 
 # Boolean Expressions
+puts 3 > 2
+puts 3 == 2
+puts 3 != 2
 
 # If Conditional Logic
+if 3 > 2
+  puts "Math works!"  
+end
+
+if 3 < 2
+  puts "Math works!"
+else
+  puts "Yikes!"
+end
 
 # If/Else Conditional Logic
+home_team = 8
+away_team = 7
+
+if home_team > away_team 
+  puts "We won!"
+else
+  puts "We lost."
+end
 
 # Elsif Conditional Logic
+home_team = 7
+away_team = 7
+
+if home_team > away_team 
+  puts "We won!"
+elsif home_team == away_team
+  puts "We tied..."
+else
+  puts "We lost."
+end
 
 # Combining Expressions
+temp = 8
+condition = "sunny" 
+
+if temp > 29 || condition == "sunny"
+  puts "Weather is great!"
+end
